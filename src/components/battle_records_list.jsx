411d1a76fbbe7bb75e90c72989982835d26412b5
DongHun Park <68440583+Laeti-Park@users.noreply.github.com>
2023-04-05 02:05:08 +0900

    Fix Battle Record Page 2🛠️

--- src/components/battle_records_list.jsx
+++ src/components/battle_records_list.jsx
@@ -1,11 +1,91 @@
 import React from "react";
-import styled from "styled-components";
 
 const matchResultArray = ['승', '무', '패'];
 const roman = ['I', 'II', 'III'];
 
 const Battles = (props) => {
     console.log(props.battles);
+
+    const battleDiv = (array, players) => {
+        return array.map(number =>
+            <div className={'battle_detail'}>
+                {
+                    players.filter(player => player.player_team === number)
+                        .map(player => {
+                            return (
+                                <div>
+                                    <div>
+                                        <img src={`/images/brawler_profile/${player.brawler_id}.webp`}
+                                             alt={'브롤러'}/>
+                                        <div>
+                                            <div>
+                                                <div>
+                                                    <img
+                                                        src={require('../images/game_icon/trophy.webp')}
+                                                        alt='트로피'/>
+                                                    {
+                                                        player.brawler_trophy
+                                                    }
+                                                </div>
+                                                <div>
+                                                    <img
+                                                        src={require('../images/game_icon/power_point.webp')}
+                                                        alt='트로피'/>
+                                                    {
+                                                        player.brawler_power
+                                                    }
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className={'battle_player_name'}>
+                                        {
+                                            player.player_name
+                                        }
+                                    </div>
+                                </div>
+                            )
+                        })
+                }
+            </div>
+        )
+    }
+
+    const battleDetail = (mode, players) => {
+
+        if (["0", "3"].includes(mode)) {
+            return (
+                <div className={'battle_1v1'}>
+                    {
+                        battleDiv(["0", "1"], players)
+                    }
+                </div>
+            )
+        } else if (mode === "2") {
+            return (
+                <div className={'battle_survive'}>
+                    {
+                        battleDiv(["0", "1", "2", "3", "4"], players)
+                    }
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <div className={'battle_survive'}>
+                        {
+                            battleDiv(["0", "1", "2", "3", "4"], players)
+                        }
+                    </div>
+                    <div className={'battle_survive'}>
+                        {
+                            battleDiv(["5", "6", "7", "8", "9"], players)
+                        }
+                    </div>
+                </div>
+            )
+        }
+    }
 
     return (
         <div className={'summary_list_2'}>
@@ -41,12 +121,7 @@ const Battles = (props) => {
                             <tbody>
                             <tr>
                                 <td colSpan={4}>
-                                    <div className={'battle_detail'}>
-                                        <div>
-                                        </div>
-                                        <div>
-                                        </div>
-                                    </div>
+                                    {battleDetail(battle.info.match_mode, battle.players)}
                                 </td>
                             </tr>
                             </tbody>
